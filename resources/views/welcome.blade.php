c506c8fcaaa07cd0996bea5cf6e46e0e31f483c9
Jolin <qooe212156822000@gmail.com>
2015-07-13 23:17:20 +0800

    create video2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,17 @@
 @extends('layout')
 
 @section('title', 'YOLO')
-
 @section('css')
 @stop
 
 @section('js')
 <!--<script src=''></script> -->
+
 @stop
 
 @section('content')
+
 content yolo
+
+
 @stop
