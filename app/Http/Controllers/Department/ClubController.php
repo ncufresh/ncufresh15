7fe34ac7e5751ae041e2075a05ec7e836d299470
Pinman <pinmanhuang@gmail.com>
2015-07-16 19:34:31 +0800

    Modify url named
    Remove backstage.
    Can put out the data from database.

--- app/Http/Controllers/Department/ClubController.php
+++ app/Http/Controllers/Department/ClubController.php
@@ -19,7 +19,7 @@ class ClubController extends Controller {
 	}
 
 	public function index() {
-		return view('department.backstage');
+		return view('department\departments');
 	}
 
 	public function store( Request $request) {
@@ -32,7 +32,7 @@ class ClubController extends Controller {
         $files = Input::file('fileName');
         foreach ($files as $file) {
             $destinationPath = 'uploads\departments';
-            $filename = $file->getClientOriginalName();
+            $filename = uniqid()."_".$file->getClientOriginalName();
             //Check file name exist or not
             while (file_exists($destinationPath."\\".$filename)) {
                 $filename = uniqid()."_".$filename;
@@ -40,72 +40,53 @@ class ClubController extends Controller {
             $upload_success = $file->move($destinationPath, $filename);
             department_pictures::create(['picName'=>$filename]);
         }
-    	
-        return redirect('/department/backstage');
+        return redirect('/department');
 	}
 
-	public function department1() {
-		$content = departments::where('category', 6)->get();
-		return view('department\all')->with('content', $content);
-	}
-
-	public function department2() {
-		$content = departments::where('category', 7)->get();
-		return view('department\all')->with('content', $content);
-	}
-
-	public function department3() {
-		$content = departments::where('category', 8)->get();
-		return view('department\all')->with('content', $content);
-	}
-
-	public function department4() {
-		$content = departments::where('category', 9)->get();
-		return view('department\all')->with('content', $content);
-	}
-
-	public function department5() {
-		$content = departments::where('category', 10)->get();
-		return view('department\all')->with('content', $content);
-	}
-
-	public function department6() {
-		$content = departments::where('category', 11)->get();
-		return view('department\all')->with('content', $content);
-	}
-
-	public function department7() {
-		$content = departments::where('category', 12)->get();
-		return view('department\all')->with('content', $content);
-	}
-
-	public function department8() {
-		$content = departments::where('category', 13)->get();
-		return view('department\all')->with('content', $content);
-	}
-
-	public function club1() {
-		$content = departments::where('category', 1)->get();
-		return view('department\all')->with('content', $content);
-	}
-
-	public function club2() {
-		$content = departments::where('category', 2)->get();
-		return view('department\all')->with('content', $content);
-	}
-
-	public function club3() {
-		$content = departments::where('category', 3)->get();
-		return view('department\all')->with('content', $content);
-	}
-
-	public function club4() {
-		$content = departments::where('category', 4)->get();
-		return view('department\all')->with('content', $content);
-	}
-
-	public function club5() {
-		$content = departments::where('category', 5)->get();
+	public function department($cate) {
+		switch ($cate) {
+			case 1:
+				$content = departments::where('category', 6)->get();
+				break;
+			case 2:
+				$content = departments::where('category', 7)->get();
+				break;
+			case 3:
+				$content = departments::where('category', 8)->get();
+				break;
+			case 4:
+				$content = departments::where('category', 9)->get();
+				break;
+			case 5:
+				$content = departments::where('category', 10)->get();
+				break;
+			case 6:
+				$content = departments::where('category', 11)->get();
+				break;
+			case 7:
+				$content = departments::where('category', 12)->get();
+				break;
+			case 8:
+				$content = departments::where('category', 13)->get();
+				break;
+			case 9:
+				$content = departments::where('category', 1)->get();
+				break;
+			case 10:
+				$content = departments::where('category', 2)->get();
+				break;
+			case 11:
+				$content = departments::where('category', 3)->get();
+				break;
+			case 12:
+				$content = departments::where('category', 4)->get();
+				break;
+			case 13:
+				$content = departments::where('category', 5)->get();
+				break;
+			default:
+				break;
+		}
 		return view('department\all')->with('content', $content);
 	}
 }
