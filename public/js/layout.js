6873492e220f628215635006289e8ad70b44c5a2
CrashedBboy <crashedbboy@gmail.com>
2015-07-16 12:45:34 +0800

    fix url

--- public/js/layout.js
+++ public/js/layout.js
@@ -5,6 +5,15 @@ $(document).ready(function(){
 			'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
 		}
 	});
+	$('.dropdown-button').dropdown({
+		inDuration: 300,
+		outDuration: 225,
+		constrain_width: true, // Does not change width of dropdown to that of the activator
+		hover: true, // Activate on hover
+		gutter: 0, // Spacing from edge
+		belowOrigin: true // Displays dropdown below the button
+		}
+	);
 	$(window).scroll(function(){
 		currentScroll = $(window).scrollTop();
 		if (currentScroll > 390){
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -16,45 +16,51 @@
 			<div class="nav-wrapper">
 				<a href="#" class="brand-logo">NCU Fresh</a>
 				<ul id="nav-mobile" class="right hide-on-med-and-down">
-					<li><a href="#">Link1</a></li>
-					<li><a href="#">Link2</a></li>
-					<li><a href="#">Link3</a></li>
-					<li><a href="#">Link4</a></li>
-					<li><a href="#">Link5</a></li>
-					<li><a href="#">Link6</a></li>
-					<li><a href="#">Link7</a></li>
-					<li><a href="#">Link8</a></li>
+					<li><a href="{{ url('campus') }}">校園導覽</a></li>
+					<li><a href="#">新生必讀</a></li>
+					<li><a href="#">中大生活</a></li>
+					<li><a href="#">常用連結</a></li>
+					<li><a href="{{ url('department')}}">系所社團</a></li>
+					<li><a href="{{ url('qa') }}">新生Q&amp;A</a></li>
+					<li><a href="{{ url('video2') }}">影音專區</a></li>
+					<li><a href="#">關於我們</a></li>
 				</ul>
 			</div>
 		</nav>
         <div id='container' class="container z-depth-2">
 			<div id='banner'>
 				<div class='row banner-menu'>
-					<div class='col s2'><a class='waves-effect waves-light btn grey darken-3' href='/campus'>校園導覽</a></div>
+					<div class='col s2'><a class='waves-effect waves-light btn grey darken-3' href='{{ url('campus') }}'>校園導覽</a></div>
 					<div class='col s2'><a class='waves-effect waves-light btn grey darken-3'>新生必讀</a></div>
 					<div class='col s4'>
-					<div>j</div>
-					<a id='logo' href='/'>logo</a>
+					<div>yo</div>
+					<a id='logo' href='{{url('/')}}'>logo</a>
 					</div>
 					<div class='col s2'><a class='waves-effect waves-light btn grey darken-3'>中大生活</a></div>
-					<div class='col s2'><a class='waves-effect waves-light btn grey darken-3'>常用連結</a></div>
+					<div class='col s2'><a class=' btn grey darken-3 dropdown-button' data-activates='banner-links'>常用連結</a></div>
+					<ul id='banner-links' class='dropdown-content'>
+						<li><a href="#!">one</a></li>
+						<li><a href="#!">two</a></li>
+						<li class="divider"></li>
+						<li><a href="#!">three</a></li>
+					</ul>
 				</div>
 				<div id='banner-img' class='row'>
 				</div>
 				<div class='row banner-menu'>
-					<div class='col s3'><a class='waves-effect waves-light btn grey darken-3' href='department/backstage'>系所社團</a></div>
+					<div class='col s3'><a class='waves-effect waves-light btn grey darken-3' href='{{ url('department')}}'>系所社團</a></div>
                     <div class='col s3'><a class='waves-effect waves-light btn grey darken-3 dropdown-button btn' data-activates='qadropdown'>新生Q&amp;A</a>
                         <ul id='qadropdown' class='dropdown-content'>
-                            <li><a href="/qa">Q&amp;A</a></li>
-                            <li><a href="/qa/create?type=qa">我要發問</a></li>
-                            <li><a href="/qa/create?type=report">問題回報</a></li>
+                            <li><a href="{{ url('qa') }}">Q&amp;A</a></li>
+                            <li><a href="{{ url('qa/create?type=qa') }}">我要發問</a></li>
+                            <li><a href="{{ url('/qa/create?type=report') }}">問題回報</a></li>
                             <li class="divider"></li>
-                            <li><a href="/qa/questions">檢視問題</a></li>
-                            <li><a href="/qa/answer">新增Q&amp;A</a></li>
+                            <li><a href="{{ url('/qa/questions') }}">檢視問題</a></li>
+                            <li><a href="{{ url('/qa/answer') }}">新增Q&amp;A</a></li>
                         </ul>
                     </div>
-					<div class='col s3'><a class='waves-effect waves-light btn grey darken-3' href='video2'>影音專區</a></div>
-					<div class='col s3'><a class='waves-effect waves-light btn grey darken-3' href='about'>關於我們</a></div>
+					<div class='col s3'><a class='waves-effect waves-light btn grey darken-3' href='{{ url('video2') }}'>影音專區</a></div>
+					<div class='col s3'><a class='waves-effect waves-light btn grey darken-3' href='{{ url('about')}}'>關於我們</a></div>
 				</div>
 			</div>
 			<div id='content'>
