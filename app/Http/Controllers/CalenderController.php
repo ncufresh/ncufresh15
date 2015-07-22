25d60cd4b6609ce94b927ae4c094b6f6e4108ca4
CrashedBboy <crashedbboy@gmail.com>
2015-07-22 22:29:54 +0800

    calender linklist

--- app/Http/Controllers/CalenderController.php
+++ app/Http/Controllers/CalenderController.php
@@ -13,28 +13,60 @@ class CalenderController extends Controller{
 
 	}
 	public function store(Request $request){
-		$pre_date = Calender::where('event_date','<',$request->date)
-					->orderBy('event_date', 'desc')->first()->event_date;
-		/*
+		$pre_date = $this->getPreviousDate($request->date);
+		$next_date = $this->getNextDate($request->date);
 		Calender::create([
 			'title'=>$request->input('title'),
 			'content'=>$request->input('content'),
-			'event_date'=>$request->date
+			'event_date'=>$request->date,
+			'previous_date'=>$pre_date,
+			'next_date'=>$next_date
 		]);
-		 */
+		$this->updateNearDate($request->date, $request->date, $pre_date, $next_date);
 		return redirect('admin');
 	}
+
 	public function update(Request $request, $id){
 		$cal = Calender::find($id);
 		$cal->title = $request->title;
 		$cal->content = $request->content;
-		$cal->event_date = $request->date;
+		if ($cal->event_date != $request->date) {
+			$pre_date = $this->getPreviousDate($request->date);
+			$next_date = $this->getNextDate($request->date);
+			$pre_date_ori = $this->getPreviousDate($cal->event_date);
+			$next_date_ori = $this->getNextDate($cal->event_date);
+			$cal->event_date = $request->date;
+			$cal->previous_date = $pre_date;
+			$cal->next_date = $next_date;
+			$this->updateNearDate($request->date, $request->date, $pre_date, $next_date);
+			$this->updateNearDate($next_date_ori, $pre_date_ori, $pre_date_ori, $next_date_ori);
+		}
 		$cal->save();
 		return redirect('admin');
 	}
+
 	public function destroy($id){
 		$cal = Calender::find($id);
+		$date = $cal->event_date;
+		$pre_date = $this->getPreviousDate($date);
+		$next_date = $this->getNextDate($date);
 		$cal->delete();
+		$this->updateNearDate($next_date, $pre_date, $pre_date, $next_date);
 		return redirect('admin');
+	}
+
+	public function getPreviousDate($now){
+		$cal = Calender::where('event_date', '<', $now)->orderBy('event_date', 'desc')->first();
+		return $cal->event_date;
+	}
+
+	public function getNextDate($now){
+		$cal = Calender::where('event_date', '>', $now)->orderBy('event_date', 'asc')->first();
+		return $cal->event_date;
+	}
+
+	public function updateNearDate($pre_new, $next_new, $pre, $next){
+		Calender::where('event_date', '=', $pre)->update(['next_date'=>$pre_new]);
+		Calender::where('event_date', '=', $next)->update(['previous_date'=>$next_new]);
 	}
 }
