23b73a58b802290a3770198b8d2f401c372bb60e
jxcodetw <jxcode.tw@gmail.com>
2015-07-23 23:36:48 +0800

    fix

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -59,7 +59,7 @@ class UserController extends Controller
     public function show(Request $request, $id)
     {
         $user = User::findOrFail($id);
-        return view('user.show', ['user' => $user]);
+        return view('user.show', ['user' => $user, 'nobreadcrumb' => true]);
     }
 
     /**
