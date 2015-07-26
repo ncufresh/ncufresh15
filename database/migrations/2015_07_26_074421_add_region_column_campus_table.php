6ed4e3cfc813816c53a693c286d73ded51c74d7b
CrashedBboy <crashedbboy@gmail.com>
2015-07-26 15:56:30 +0800

    fix migration

--- database/migrations/2015_07_26_074421_add_region_column_campus_table.php
+++ database/migrations/2015_07_26_074421_add_region_column_campus_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class UpdateCampusTable extends Migration
+class AddRegionColumnCampusTable extends Migration
 {
     /**
      * Run the migrations.
