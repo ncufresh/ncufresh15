aad536d11c2a8c885e0a9b0f04300f59c84e3d37
Jolin <qooe212156822000@gmail.com>
2015-07-28 14:53:04 +0800

    nothing

--- resources/views/video/video2.blade.php
+++ resources/views/video/video2.blade.php
@@ -26,7 +26,7 @@
     /* Make the CDN fonts accessible from the CSS */
 }
 .unused{
-    color:#ffffff;
+    color:black;
 }
 
   @font-face {
@@ -39,6 +39,8 @@
 
   /* Make the demo a little prettier */
   .video-js { margin: 20px auto; }
+
+
 
 </style>
 
@@ -61,7 +63,7 @@ $(document).ready(function(){
        success: function(msg) {
         console.log(msg);   //js, save data
         var id = msg.id;
-        var str = '<div id="'+msg.id+'" class="card-action , unused"> \
+        var str = '<div id="'+msg.id+'" class="card-action "> \
                       <div class="col s2">'+msg.name+'</div>\
                       <div class="col s7">留言內容 '+msg.comment+'</div>\
                       <div class="col s2">\
@@ -100,20 +102,13 @@ $(document).ready(function(){
       $(".unused").hide();
       $(".unused:first").show();
 
-    $(window).scroll(function() {   
-      if($(window).scrollTop() + $(window).height() > $(document).height() - 50) {
-      
-           $(".unused:first").removeClass("unused");
-           $(".unused:first").fadeIn(3000);
+      $(".next").click(function(){
+          $(".unused:first").removeClass("unused");
+          $(".unused:first").fadeIn(1000);
+       });
 
-       }  
-    });
+
   });
-
-
-
-
-
 });
 </script>
 
@@ -124,75 +119,78 @@ $(document).ready(function(){
 <!--最上面那條row(red accent-2)-->    
 <div class="row">
 <!--有影片的section( teal lighten-2)-->    
-      <div class="col s8" style=";">
+  <div class="col s8" style=";">
 
 
-  <video id="my_video_1" class="video-js vjs-default-skin" 
+    <video id="my_video_1" class="video-js vjs-default-skin" 
       controls preload="none" width="550px" height="400px" data-setup='{}'
       poster='http://video-js.zencoder.com/oceans-clip.jpg'>
-    <source src="http://vjs.zencdn.net/v/oceans.mp4" type='video/mp4' />
-    <source src="http://vjs.zencdn.net/v/oceans.webm" type='video/webm' />
-  </video>
-          <p>影片簡介：</p>
-          <p>一一一一</p>
-      </div>
+      <source src="http://vjs.zencdn.net/v/oceans.mp4" type='video/mp4' />
+      <source src="http://vjs.zencdn.net/v/oceans.webm" type='video/webm' />
+    </video>
+    <p>影片簡介：</p>
+    <p>一一一一</p>
+  </div>
 <!--留言的Ssection( orange accent-2)-->    
-      <div class="col s4 " style=";" >
-         <div class="card blue-grey darken-1 hoverable" style=";">
-            <div class="card-content white-text" style="height:85%;">
-              <span class="card-title">留言板</span>
-
-              <div id="place"></div>
-
-            @foreach($tryconnect as $try)
-            
-      <div id="{{$try->id}}"> 
-         <div class="col s2">{{$try->name}}</div>
+  <div class="col s4 " style=";" >
+    <div class="card blue-grey darken-1 hoverable" style=";">
+        <div class="card-content white-text" style="height:650px;overflow-y:scroll;">
+          <span class="card-title">留言板</span>
+          <div id="place"></div>  
+<?php $i = 0; ?>
+@foreach($tryconnect as $try)
+<?php 
+if ( $i % 8 == 0) {
+echo<<<_END
+  <div class="unused">
+_END;
+} ?>
+          <div id="{{$try->id}}"> 
+            <div class="col s2">{{$try->name}}</div>
             <div class="col s7">留言內容 {{$try->comment}}</div>
-              <div class="col s2">
-                <form action="comment(delete).php" method="post">
-                  <button type="submit" class="waves-effect waves-teal btn , delete" value="{{ $try->id }}" style="width:8px;height:25px;margin-left:-10px">
-                    <i class="material-icons" style="margin-left:-8px;line-height: normal;">delete</i>
-                  </botton>
-                </form>
-              </div>
+            <div class="col s2">
+              <form>
+                <button type="submit" class="waves-effect waves-teal btn , delete" value="{{ $try->id }}" style="width:8px;height:25px;margin-left:-10px">
+                  <i class="material-icons" style="margin-left:-8px;line-height: normal;">delete</i>
+                </botton>
+              </form>
             </div>
-           <div class="row" style="margin-bottom: 15px;margin-top: 0px;">  </div>
-            @endforeach
+          </div>
+<?php 
+if( (++$i) % 8 == 0 ) {
+echo<<<_END
+<div>
+<br>
+<button type="submit" class="next">next 8 messenges</button>
+_END;
+} ?>
+<div class="row" style="margin-bottom: 15px;margin-top: 0px;">  </div>
+@endforeach
 
-
-
-            </div>
-            <div class="card-action">
-            <div class="col s9">
+<?php
+if($i % 8 != 8){
+  echo "</div>";
+}
+?>
+        </div></div></div>
+        <div class="card-action" style="height:100px;overflow:hidden;">
+          <div class="col s9">
             <p style="color:white">Message:</p>
             <form method="post" >
-            {!! csrf_field() !!}
-            <input type="text" id="SendComment" name="comment" style="background-color: white; width:100%;height:25px;margin-left:-5px">
-            </div>
-            <div class="col s3">
+              {!! csrf_field() !!}
+              <input type="text" id="SendComment" name="comment" style="background-color: white; position:absolute; width:55%;height:25px;margin-left:-10px; margin-bottom:30px">
+          </div>
+          <div class="col s3">
             <p>  </p>
             <br>
-            <button type="submit" id="send" style="width:20px;height:25px;margin-top:12px;margin-left:-10px;" class="waves-effect waves-light btn" >
+            <button type="submit" id="send" style="position:absolute;width:20px;height:25px;margin-left:-10px;margin-bottom:30px" class="waves-effect waves-light btn" >
               <i class="material-icons" style="margin-left:-5px;line-height: normal;">send</i>
             </button>
-            </div>
-            </div>
-            </form>
           </div>
-         </div>
+        </div>
+        </form>
+      </div>
     </div>
-
+  </div>
 </div>
-    </body>
-  </html>
-
-
-
-
-
-
-content yolo!!!!
-
-
 @stop
