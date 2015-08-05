99b5ef1a0447ae7de58591fed55a89adee897a7b
CrashedBboy <crashedbboy@gmail.com>
2015-08-05 19:57:27 +0800

    fix user side menu

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,7 +7,6 @@
 		<!--Let browser know website is optimized for mobile-->
       	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 		<meta name="csrf-token" content="{{ csrf_token() }}">
-		<link rel="shortcut icon" href="{{asset('favicon.ico')}}" type="image/x-icon">
 		<link rel="icon" href="{{asset('icon.png')}}" type="image/png">
 		<meta charset="UTF-8">
 		<meta property="og:title" content="中央大學|2015 新生知訊網" />
