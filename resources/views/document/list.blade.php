fe18893de90a45e3c9bd1a88c752ff41585e972f
ncufresh <ncufreshweb@gmail.com>
2015-08-05 22:53:57 +0800

    fix list

--- resources/views/document/list.blade.php
+++ resources/views/document/list.blade.php
@@ -11,34 +11,34 @@
 	<h5 class="page_title">學習及生活相關須知</h5>
 	@endif
 @else
-	@if ($page_id === "university" && $page_id_2 === 1)
+	@if ($page_id === "university" && $page_id_2 == 1)
 	<h5 class="page_title">重要日程</h5>
-	@elseif($page_id === "university" && $page_id_2 === 2)
+	@elseif($page_id === "university" && $page_id_2 == 2)
 	<h5 class="page_title">學籍</h5>
-	@elseif($page_id === "university" && $page_id_2 === 3)
+	@elseif($page_id === "university" && $page_id_2 == 3)
 	<h5 class="page_title">繳費</h5>
-	@elseif($page_id === "university" && $page_id_2 === 4)
+	@elseif($page_id === "university" && $page_id_2 == 4)
 	<h5 class="page_title">兵役</h5>
-	@elseif($page_id === "university" && $page_id_2 === 5)
+	@elseif($page_id === "university" && $page_id_2 == 5)
 	<h5 class="page_title">住宿</h5>
-	@elseif($page_id === "university" && $page_id_2 === 6)
+	@elseif($page_id === "university" && $page_id_2 == 6)
 	<h5 class="page_title">學生證</h5>
-	@elseif($page_id === "university" && $page_id_2 === 7)
+	@elseif($page_id === "university" && $page_id_2 == 7)
 	<h5 class="page_title">其他注意事項</h5>
 
-	@elseif($page_id === "graduate" && $page_id_2 === 1)
+	@elseif($page_id === "graduate" && $page_id_2 == 1)
 	<h5 class="page_title">重要日程</h5>
-	@elseif($page_id === "graduate" && $page_id_2 === 2)
+	@elseif($page_id === "graduate" && $page_id_2 == 2)
 	<h5 class="page_title">學籍</h5>
-	@elseif($page_id === "graduate" && $page_id_2 === 3)
+	@elseif($page_id === "graduate" && $page_id_2 == 3)
 	<h5 class="page_title">繳費</h5>
-	@elseif($page_id === "graduate" && $page_id_2 === 4)
+	@elseif($page_id === "graduate" && $page_id_2 == 4)
 	<h5 class="page_title">兵役</h5>
-	@elseif($page_id === "graduate" && $page_id_2 === 5)
+	@elseif($page_id === "graduate" && $page_id_2 == 5)
 	<h5 class="page_title">住宿</h5>
-	@elseif($page_id === "graduate" && $page_id_2 === 6)
+	@elseif($page_id === "graduate" && $page_id_2 == 6)
 	<h5 class="page_title">學生證</h5>
-	@elseif($page_id === "graduate" && $page_id_2 === 7)
+	@elseif($page_id === "graduate" && $page_id_2 == 7)
 	<h5 class="page_title">其他注意事項</h5>
 	@endif
 @endif
