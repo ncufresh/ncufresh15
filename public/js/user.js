a9a34b5efeec7411973b151be489ceefc93148d9
CrashedBboy <crashedbboy@gmail.com>
2015-08-05 21:07:53 +0800

    yo

--- public/js/user.js
+++ public/js/user.js
@@ -1,3 +1,14 @@
+var c1, c2, c3, c4;
+c1 = new Fish("c1");
 $(document).ready(function(){
 	$(".button-collapse").sideNav();
+	for (i = 0; i < 3; i++){
+		$.ajax({
+			url: "/creature/info/"+ i,
+			type: "GET",
+			success: function(data){
+				console.log();
+			}
+		});
+	}
 });
